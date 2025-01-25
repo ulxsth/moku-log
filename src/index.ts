a5289277ff8ed5a30872282bc6046f2000026ec7
ulxsth <ulxsth@gmail.com>
2025-01-25 22:09:14 +0900

    chore: bot の起動スクリプトを作成

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { Client, GatewayIntentBits } from "discord.js";
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const BOT_TAG = client.user?.tag;
+
+client.once('ready', () => {
+  console.log(`${BOT_TAG} としてログインします...`);
+
+  console.log("ログインに成功しました");
+});
+
+// 起動処理
+const token = process.env.DISCORD_TOKEN;
+if(!token) {
+  console.error('環境変数 "DISCORD_TOKEN" が設定されていません');
+  process.exit(1);
+}
+client.login(token);
