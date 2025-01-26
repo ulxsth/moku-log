1f260c62e12305c021aa93d0e6319e494871945c
ulxsth <ulxsth@gmail.com>
2025-01-26 18:38:16 +0900

    feat: コマンドの一括登録処理を index.ts に実装しなおした

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,45 @@
-import { Client, GatewayIntentBits } from "discord.js";
+import { Client, Collection, GatewayIntentBits, REST, Routes } from "discord.js";
+import type DiscordCommand from "./interfaces/discordCommand";
+import path from "path";
+import fs from "fs";
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-const BOT_TAG = client.user?.tag;
+
+const commands = new Collection<string, DiscordCommand>()
+const commandsJSON: any[] = []
 
 client.once('ready', () => {
-  console.log(`${BOT_TAG} としてログインしました`);
+  console.log(`${client.user?.tag} としてログインしました`);
+
+  const commandsDir = path.resolve("./commands");
+  const commandFiles = fs.readdirSync(commandsDir).filter(file => file.endsWith(".ts"));
+
+  Promise.all(
+    commandFiles.map(async (file) => {
+      const filePath = path.join(commandsDir, file);
+      const command = await import(filePath)
+      if ('data' in command && 'execute' in command) {
+        commands.set(command.data.name, command);
+      } else {
+        console.log(`[WARN] ${file} には data または execute が実装されていないため、読み込みをスキップします`);
+      }
+    })
+  )
+    .then(async () => {
+      const token = process.env.DISCORD_TOKEN!;
+      const appId = process.env.DISCORD_APP_ID!;
+      const guildId = process.env.DISCORD_GUILD_ID!;
+
+      const rest = new REST().setToken(token);
+      const data = await rest.put(Routes.applicationGuildCommands(appId, guildId), { body: commandsJSON }) as any;
+
+      console.log(`${data.length} コマンドの更新が完了しました。`);
+    })
 });
 
 // 起動処理
 const token = process.env.DISCORD_TOKEN;
-if(!token) {
+if (!token) {
   console.error('環境変数 "DISCORD_TOKEN" が設定されていません');
   process.exit(1);
 }
