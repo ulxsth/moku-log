b3f39455573dc037b1f18fc6790ac5b0cb1008ba
ulxsth <ulxsth@gmail.com>
2025-01-28 10:14:22 +0900

    feat: /create に応じてイベント作成フォームを表示するように実装

--- src/commands/create.ts
+++ src/commands/create.ts
@@ -1,13 +1,55 @@
-import { SlashCommandBuilder } from '@discordjs/builders';
-import { CommandInteraction } from 'discord.js';
+import { ActionRowBuilder, ModalBuilder, SlashCommandBuilder, TextInputBuilder, type AnyComponentBuilder } from '@discordjs/builders';
+import { CommandInteraction, TextInputStyle } from 'discord.js';
 
 const CMD_NAME = 'create';
-const CMD_DESCRIPTION = '新しいアクティビティを作成します（実行後に登録フォームが表示されます）';
+const CMD_DESCRIPTION = '新しいイベントを作成します（実行後に登録フォームが表示されます）';
 
 export const data = new SlashCommandBuilder()
   .setName(CMD_NAME)
   .setDescription(CMD_DESCRIPTION);
 
 export const execute = async (interaction: CommandInteraction) => {
-  await interaction.reply('test');
+  const modal = createModal();
+  await interaction.showModal(modal);
+
+  // 入力送信後の処理は ModalSubmitInteraction リスナで行う
+}
+
+const createModal = (): ModalBuilder => {
+  const actionRows: ActionRowBuilder<TextInputBuilder>[] = [];
+
+  const title = new TextInputBuilder()
+    .setCustomId('activityTitle')
+    .setLabel('イベント名')
+    .setStyle(TextInputStyle.Short)
+    .setRequired(true);
+    actionRows.push(new ActionRowBuilder<TextInputBuilder>().addComponents(title));
+
+  const description = new TextInputBuilder()
+    .setCustomId('activityDescription')
+    .setLabel('イベントの説明')
+    .setStyle(TextInputStyle.Paragraph);
+    actionRows.push(new ActionRowBuilder<TextInputBuilder>().addComponents(description));
+
+  const date = new TextInputBuilder()
+    .setCustomId('activityDate')
+    .setLabel('日時')
+    .setPlaceholder('YYYY/MM/DD HH:MM')
+    .setStyle(TextInputStyle.Short)
+    .setRequired(true);
+    actionRows.push(new ActionRowBuilder<TextInputBuilder>().addComponents(date));
+
+  const channeiId = new TextInputBuilder()
+    .setCustomId('activityChannelId')
+    .setLabel('イベントチャンネル（ID）')
+    .setStyle(TextInputStyle.Short)
+    .setRequired(true);
+    actionRows.push(new ActionRowBuilder<TextInputBuilder>().addComponents(channeiId));
+
+  const modal = new ModalBuilder()
+    .setCustomId('createActivity')
+    .setTitle('イベントの作成');
+  modal.addComponents(...actionRows);
+
+  return modal;
 }
