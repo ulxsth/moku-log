65e9a24fef69e29c96d34660af7c109e76309564
ulxsth <ulxsth@gmail.com>
2025-02-02 14:49:52 +0900

    fix: jest 内で @ アノテーションが使えるように修正

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
+import fs from "fs";
+import { pathsToModuleNameMapper } from 'ts-jest';
+
+const tsconfig = JSON.parse(fs.readFileSync("./tsconfig.json", "utf8"));
+
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 export default {
   preset: "ts-jest",
   testEnvironment: "node",
   roots: ["<rootDir>/tests", "<rootDir>/src"],
+  moduleNameMapper: pathsToModuleNameMapper(tsconfig.compilerOptions.paths, { prefix: "<rootDir>/" })
 };
